40ba7af2e17612f70ebb9b7b4ce23c7435eec1b0
Tobias Bieniek <tobias@bieniek.cloud>
2026-09-05 18:32:03 +0200

    polar: Add wind-aware glide solver

--- libs/updraft_polar/src/lib.rs
+++ libs/updraft_polar/src/lib.rs
@@ -12,10 +12,12 @@
 
 mod atmosphere;
 mod coefficients;
+mod glide;
 mod glide_polar;
 mod store;
 
 pub use atmosphere::isa_density_ratio;
 pub use coefficients::PolarCoefficients;
+pub use glide::GlideSolution;
 pub use glide_polar::GlidePolar;
 pub use store::{POLAR_STORE, PolarStoreEntry};
